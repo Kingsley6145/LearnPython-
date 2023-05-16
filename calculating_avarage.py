a2892d2da019f417745663a128b3f323e3b6d505
Kingsley6145 <87859909+Kingsley6145@users.noreply.github.com>
2023-05-16 07:26:22 +0300

    Add files via upload

---
+++ calculating_avarage.py
@@ -0,0 +1,12 @@
+# This is a comment
+# Prompt the user to enter the first number
+num1 = float(input("Enter the first number: "))
+
+# Prompt the user to enter the second number
+num2 = float(input("Enter the second number: "))
+
+# Calculate the average
+average = (num1 + num2) / 2
+
+# Print the average
+print("The average of", num1, "and", num2, "is", average)
