17bb26997a285f4cfaad0075dd1a6f984b970083
Kingsley6145 <87859909+Kingsley6145@users.noreply.github.com>
2023-05-29 12:27:48 +0300

    Add files via upload

---
+++ accessing items in a dictionary.py
@@ -0,0 +1,9 @@
+student = {
+    "name": "Kingsley",
+    "age": 22,
+    "email": "kingsleytrapking@gmail.com"
+}
+a = student["name"]
+b = student.get("age")
+print(a)
+print(b)
---
+++ get keys,values and items.py
@@ -0,0 +1,13 @@
+student = {
+    "name": "Kingsley",
+    "age": 22,
+    "email": "kingsleytrapking@gmail.com"
+}
+keys = student.keys()
+print("Keys:", keys)
+
+values = student.values()
+print("Values:", values)
+
+items = student.items()
+print("Items:", items)
