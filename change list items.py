3fe6dafa3fbb894d0979e00fbdbcdc1a9fcfe4a4
Kingsley6145 <87859909+Kingsley6145@users.noreply.github.com>
2023-05-29 12:03:38 +0300

    Add files via upload

---
+++ change list items.py
@@ -0,0 +1,6 @@
+fruits = ["apple", "banana", "orange", "grapes"]
+print("Original list:", fruits)
+fruits[1] = "mango"
+print("Updated list:", fruits)
+fruits[1:3] = ["pineapple", "watermelon"]
+print("Updated list:", fruits)
